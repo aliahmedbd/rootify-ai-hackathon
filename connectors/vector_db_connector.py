f5493680cf1486c76d21b7aac0633c4021d85f28
Akshaya1009 <akshaya10092000@gmail.com>
2025-05-07 13:33:35 +0530

    added the search using pymilvus

--- connectors/vector_db_connector.py
+++ connectors/vector_db_connector.py
@@ -9,3 +9,35 @@ print("\n🔍 Search Results:")
 for i, res in enumerate(results, 1):
     print(f"\nResult {i}:\n{res.page_content}\n")
 
+# from pymilvus import connections, Collection
+# from dotenv import load_dotenv
+# import os
+
+# # Load environment variables from .env file
+# load_dotenv()
+
+# # Connect to Milvus
+# connections.connect(
+#     alias="default",
+#     uri=f"grpc://{os.environ['grpcHost']}:{os.environ['grpcPort']}",
+#     secure=True,
+#     user=os.environ['milvusUser'],
+#     password=os.environ['milvusPass']
+# )
+# print("✅ Connected to Milvus!")
+
+# # Name of the existing collection
+# collection_name = "DevOpsAssist"
+
+# # Load the collection
+# collection = Collection(name=collection_name)
+# collection.load()
+# print(f"📦 Collection '{collection_name}' loaded.")
+
+
+# results = collection.query(
+#     expr="source != ''",
+#     output_fields=["source"],
+#     limit=10
+# )
+# print(results)
