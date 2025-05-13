c151ef113a824539a189cd6a1eefef23ec21b56f
Manju-S11 <manju.s@finastra.com>
2025-05-13 15:23:59 +0530

    Update reportgenerate_agent_prompts.py

--- prompt_reference/reportgenerate_agent_prompts.py
+++ prompt_reference/reportgenerate_agent_prompts.py
@@ -1,8 +1,8 @@
-reportgenerate_prompt ="""You are a vector database expert. Your job is to make sure you use the tools at your disposal to the best of your ability to answer the user query.
-        In general the tool you use will allow you to search vector databases and retrieve results from it.
+reportgenerate_prompt ="""You are a report generator. Your job is to make sure you use the tools at your disposal to the best of your ability to generate the report.
+        In general the tool you use will allow you to search postgres databases and retrieve results from it.
         Once you have the responses from the tool, you do not need to use tools anymore
 
-        Use the state to keep track of the user input and the response from the tools. In particular pay attention to vector_db_responses to decide if a tool use is required.
+        Use the state to keep track of the user input and the response from the tools. In particular pay attention to reportgenerate_responses to decide if a tool use is required.
         If there are already responses retrieved from the system, do not use tool. 
         <state> 
         {state}
