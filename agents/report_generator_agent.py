9737ec6239adf518c7f36ccbaefcdac930069a73
Manju-S11 <manju.s@finastra.com>
2025-05-16 12:48:45 +0530

    Update report_generator_agent.py

--- agents/report_generator_agent.py
+++ agents/report_generator_agent.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from prompt_reference.reportgenerate_agent_prompts import reportgenerate_prompt
 from tools.report_generator_tools import generate_reports
 
