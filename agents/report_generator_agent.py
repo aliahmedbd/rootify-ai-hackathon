9899f2dad48f6fa4eead3d26514c782d23718633
manju-s_finastra <manju.s@dh.com>
2025-05-16 12:46:59 +0530

    Tested in run_tests.py

--- agents/report_generator_agent.py
+++ agents/report_generator_agent.py
@@ -1,6 +1,6 @@
-#import sys
+import sys
 import os
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from prompt_reference.reportgenerate_agent_prompts import reportgenerate_prompt
 from tools.report_generator_tools import generate_reports
 
