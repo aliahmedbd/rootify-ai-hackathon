e2b4962f578a66982b36629ff5bba31a76c01419
nandeeshmm <nandeesh.mm@finastra.com>
2025-05-22 22:44:13 +0530

    Updating the tools

--- tests/test_postgress_tool.py
+++ tests/test_postgress_tool.py
@@ -1,16 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-<<<<<<< HEAD
 from tools.postgres_agent_tools import PostGresAgentTools
-<<<<<<< HEAD
-#from tools.postgres_agent_tools import get_table_schemas
-from tools.postgres_agent_tools import get_table_schemas
-=======
-=======
-from tools.postgres_agent_tools import get_table_schemas
->>>>>>> 007fbf1 (Getting all the schema name from tabel)
->>>>>>> 3f9961a (Getting all the schema name from tabel)
 from connectors.db_connector import PostgresConnector   
 
 def test_postgres_tool():
