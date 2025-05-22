c7185b099fbb4cda457695a5af632fb3d0b533da
nandeeshmm <nandeesh.mm@finastra.com>
2025-05-22 17:36:52 +0530

    modified the script

--- playground.py
+++ playground.py
@@ -1,7 +1,10 @@
-from langchain.prompts import ChatPromptTemplate
-from langchain.agents import Tool
-from langchain_ibm import ChatWatsonx
-from langchain.agents import initialize_agent
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# from langchain.prompts import ChatPromptTemplate
+# from langchain.agents import Tool
+# from langchain_ibm import ChatWatsonx
+# from langchain.agents import initialize_agent
+# import os
 from connectors.db_connector import PostgresConnector
 clinet = PostgresConnector()
